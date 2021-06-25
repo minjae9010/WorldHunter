1ae7e8827c182951b9bc0ac453d76953bac26015
BuildTools <unconfigured@null.spigotmc.org>
2021-06-25 15:02:39 +0900

    HashMap을 사용한 팀 데이터 저장

--- src/main/java/tk/mjsv/YAML.java
+++ src/main/java/tk/mjsv/YAML.java
@@ -2,6 +2,7 @@ package tk.mjsv;
 
 import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 
@@ -19,23 +20,23 @@ public class YAML {
 
     public static void loadData(){
         teamData = YamlConfiguration.loadConfiguration(team);
+        try {
+            if (!team.exists()) {
+                teamData.save(team);
+                teamData.load(team);
 
-        if(!team.exists()){
-            try {
-                team.createNewFile();
-            } catch (IOException e) {
-                Bukkit.getLogger().warning(e.toString());
-            }
-        }
-        else{
-            teamList = teamData.getStringList("teamlist");
-            for(String tl:teamList) {
-                ArrayList<OfflinePlayer> sl = teamHash.get(tl);
-                for (String pl:teamData.getStringList(tl)){
-                    sl.add(Bukkit.getOfflinePlayerIfCached(pl));
+            } else {
+                teamList = teamData.getStringList("teamlist");
+                for (String tl : teamList) {
+                    ArrayList<OfflinePlayer> sl = teamHash.get(tl);
+                    for (String pl : teamData.getStringList(tl)) {
+                        sl.add(Bukkit.getOfflinePlayerIfCached(pl));
+                    }
+                    teamHash.put(tl, sl);
                 }
-                teamHash.put(tl,sl);
             }
+        } catch (InvalidConfigurationException|IOException e) {
+            e.printStackTrace();
         }
     }
 }
