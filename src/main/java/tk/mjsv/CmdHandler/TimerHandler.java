327e0ff064615cb36db8c5959febc0eea3dcb1f0
BuildTools <unconfigured@null.spigotmc.org>
2021-06-26 12:57:16 +0900

    타이머 추가2

---
+++ src/main/java/tk/mjsv/CmdHandler/TimerHandler.java
@@ -0,0 +1,85 @@
+package tk.mjsv.CmdHandler;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import tk.mjsv.EventHanler.Timer;
+import tk.mjsv.WorldHunter;
+
+public class TimerHandler {
+    private static final String index = WorldHunter.index;
+    private static String label = "타이머";
+    private static WorldHunter pl;
+
+    public static int seconds = 0;
+    public static int Pseconds = 0;
+    public static int Wseconds = 0;
+    public static int taskid;
+    public static boolean TimerStop = false;
+    public static String Tset = "없음";
+
+    public static void Command(CommandSender sender, String[] args) {
+        if (sender.isOp()) {
+            if (args.length == 1) {
+                if (args[0].equals("설정")) {
+                    sender.sendMessage(index + "§e/" + label + " 설정 <내용> <분> <초>");
+                }
+                if (args[0].equals("시작")) {
+                    if (!(Pseconds == 0) & !(Wseconds == 0)) {
+                        if (Timer.set) {
+                            sender.sendMessage(index + "이미 타이머가 시작되었습니다");
+                        } else {
+                            sender.sendMessage(index + "타이머가 시작됩니다");
+                            taskid = Bukkit.getScheduler().scheduleSyncRepeatingTask(pl, new Timer(), 0, 20);
+                        }
+                    } else {
+                        if (Pseconds == 0) {
+                            sender.sendMessage(index + "평화시간이 설정되지 않았습니다.");
+                        }
+                        if (Wseconds == 0) {
+                            sender.sendMessage(index + "전쟁시간이 설정되지 않았습니다.");
+                        }
+                    }
+                }
+                if (args[0].equals("종료")) {
+                    if (Timer.set) {
+                        sender.sendMessage(index + "타이머가 곧 종료됩니다");
+                        TimerStop = true;
+                    } else {
+                        sender.sendMessage(index + "타이머가 시작되지 않았습니다");
+                    }
+                }
+            } else if (args.length >= 2 & args.length <= 6) {
+                if (args[0].equals("설정")) {
+                    if (args.length == 2) {
+                        Tset = args[1];
+                        sender.sendMessage(index + "시간을 정해주세요");
+                    } else if (args.length == 3) {
+                        Tset = args[1];
+                        seconds = Integer.parseInt(args[2]) * 60;
+                        sender.sendMessage(index + Tset + " " + seconds / 60 + "분 이 설정되었습니다");
+                    } else if (args.length == 4) {
+                        Tset = args[1];
+                        seconds = Integer.parseInt(args[2]) * 60 + Integer.parseInt(args[3]);
+                        sender.sendMessage(index + Tset + " " + seconds / 60 + "분 " + seconds % 60 + "초가 설정되었습니다");
+                    }
+
+                }
+                if (!Tset.equals("없음"))
+                    switch (Tset) {
+                        case "전쟁":
+                            Wseconds = seconds;
+                            break;
+                        case "평화":
+                            Pseconds = seconds;
+                            break;
+                        default:
+                            sender.sendMessage(index + "없는 타입입니다");
+                    }
+            } else {
+                sender.sendMessage(index + "§e/" + label + " 설정 <타입> <분> <초>");
+                sender.sendMessage(index + "§e/" + label + " 시작");
+                sender.sendMessage(index + "§e/" + label + " 종료");
+            }
+        }
+    }
+}
---
+++ src/main/java/tk/mjsv/EventHanler/Timer.java
@@ -0,0 +1,80 @@
+package tk.mjsv.EventHanler;
+
+import org.bukkit.Bukkit;
+import tk.mjsv.CmdHandler.TimerHandler;
+import tk.mjsv.WorldHunter;
+
+public class Timer implements Runnable {
+    private static final String index = WorldHunter.index;
+    public static int count = -1;
+    private static String str;
+    public static boolean set = false;
+    public static String setting = "없음";
+
+    @Override
+    public void run() {
+        if (!set) {
+            count = TimerHandler.Pseconds;
+            setting = "평화";
+            set = true;
+        }
+        switch (setting) {
+            case "평화":
+                str = "평화시간";
+                break;
+            case "전쟁":
+                str = "전쟁시간";
+        }
+
+        switch (count) {
+            case 5400:
+                Bukkit.broadcastMessage(index + " " + str + " : 1시간 30분 남았습니다.");
+                break;
+            case 3600:
+                Bukkit.broadcastMessage(index + " " + str + " : 1시간 남았습니다.");
+                break;
+            case 1800:
+                Bukkit.broadcastMessage(index + " " + str + " : 30분 남았습니다.");
+                break;
+            case 1500:
+                Bukkit.broadcastMessage(index + " " + str + " : 25분 남았습니다.");
+                break;
+            case 1200:
+                Bukkit.broadcastMessage(index + " " + str + " : 20분 남았습니다.");
+                break;
+            case 900:
+                Bukkit.broadcastMessage(index + " " + str + " : 15분 남았습니다.");
+                break;
+            case 600:
+                Bukkit.broadcastMessage(index + " " + str + " : 10분 남았습니다.");
+                break;
+            case 300:
+                Bukkit.broadcastMessage(index + " " + str + " : 5분 남았습니다.");
+                break;
+            case 120:
+                Bukkit.broadcastMessage(index + " " + str + " : 2분 남았습니다.");
+                break;
+        }
+
+        if (count <= 5) {
+            Bukkit.broadcastMessage(index + " " + str + " : " + count + "초 남았습니다.");
+        }
+        if (count == 0) {
+            Bukkit.broadcastMessage(index + " " + str + "이 종료되었습니다");
+            if (setting.equals("평화")) {
+                count = TimerHandler.Wseconds;
+                setting = "전쟁";
+            } else if (setting.equals("전쟁")) {
+                set = false;
+                Bukkit.getScheduler().cancelTask(TimerHandler.taskid);
+            }
+        }
+        if (TimerHandler.TimerStop) {
+            TimerHandler.TimerStop = false;
+            Bukkit.broadcastMessage(index + "관리자가 타이머를 종료 하였습니다");
+            set = false;
+            Bukkit.getScheduler().cancelTask(TimerHandler.taskid);
+        }
+        count--;
+    }
+}
